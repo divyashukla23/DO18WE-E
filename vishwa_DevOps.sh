f774ed7ef43672eeb2a3917d8a92f2bf79780956
vishwa75 <vishwak757@gmail.com>
2024-01-06 11:42:27 +0530

    first commit vishwa

---
+++ vishwa_DevOps.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "PEOPLE WHO CONTINUE TO PUT"
+echo "THEIR LIVES ON THE LINE TO DEFEND"
+echo "THEIR FAITH BECOME HEROES AND"
+echo "CONTINUE TO EXIST ON IN LEGEND."
+echo "– NARUTO"
+
+This commit is from vishwakkanna
